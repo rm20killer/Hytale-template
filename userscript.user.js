04c185d64e560a8fe8947a3ca7ccb5678a9109d5
RM20 <43253883+rm20killer@users.noreply.github.com>
2022-04-03 02:47:18 +0100

    updates

--- userscript.user.js
+++ userscript.user.js
@@ -13,7 +13,7 @@ if (window.top !== window.self) {
             document.getElementsByTagName("mona-lisa-embed")[0].shadowRoot.children[0].getElementsByTagName("mona-lisa-canvas")[0].shadowRoot.children[0].appendChild(
         (function () {
             const i = document.createElement("img");
-            i.src = "https://cdn.discordapp.com/attachments/448474274219425795/959959092200374362/dotted_template.png";
+            i.src = "https://cdn.discordapp.com/attachments/959166566954569758/959991634223841380/dotted_template2.png";
             i.style = "position: absolute;left: 0;top: 0;image-rendering: pixelated;width: 2000px;height: 1000px;";
             console.log(i);
             return i;
